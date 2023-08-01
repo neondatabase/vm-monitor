a102b746b0deb2f5a3e31408f757885ec5d79cab
Felix Prasanna <felix@neon.tech>
2023-08-01 16:57:02 -0400

    fix comments

--- src/cgroup.rs
+++ src/cgroup.rs
@@ -108,7 +108,7 @@ where
 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("PeekableStream")
-            .field("stream", &"{ .. }" as &dyn Debug)
+            .field("stream", &"{ .. }")
             .field("peek", &self.peek)
             .finish()
     }
@@ -162,14 +162,14 @@ where
 /// Configuration for a `CgroupWatcher`
 #[derive(Debug, Clone)]
 pub struct Config {
-    // oom_buffer_bytes gives the target difference between the total memory reserved for the cgroup
+    // The target difference between the total memory reserved for the cgroup
     // and the value of the cgroup's memory.high.
     //
-    // In other words, memory.high + OOMBufferBytes will equal the total memory that the cgroup may
+    // In other words, memory.high + oom_buffer_bytes will equal the total memory that the cgroup may
     // use (equal to system memory, minus whatever's taken out for the file cache).
     oom_buffer_bytes: u64,
 
-    // memory_high_buffer_bytes gives the amount of memory, in bytes, below a proposed new value for
+    // The amount of memory, in bytes, below a proposed new value for
     // memory.high that the cgroup's memory usage must be for us to downscale
     //
     // In other words, we can downscale only when:
@@ -180,25 +180,25 @@ pub struct Config {
     // memory in use by the kernel's page cache that we're actually ok with getting rid of.
     pub(crate) memory_high_buffer_bytes: u64,
 
-    // max_upscale_wait gives the maximum duration, in milliseconds, that we're allowed to pause
+    // The maximum duration, in milliseconds, that we're allowed to pause
     // the cgroup for while waiting for the autoscaler-agent to upscale us
     max_upscale_wait: Duration,
 
-    // do_not_freeze_more_often_than gives a required minimum time, in milliseconds, that we must
-    // wait before re-freezing the cgroup while waiting for the autoscaler-agent to upscale us.
+    // The required minimum time, in milliseconds, that we must wait before re-freezing
+    // the cgroup while waiting for the autoscaler-agent to upscale us.
     do_not_freeze_more_often_than: Duration,
 
-    // memory_high_increase_by_bytes gives the amount of memory, in bytes, that we should periodically
-    // increase memory.high by while waiting for the autoscaler-agent to upscale us.
+    // The amount of memory, in bytes, that we should periodically increase memory.high
+    // by while waiting for the autoscaler-agent to upscale us.
     //
     // This exists to avoid the excessive throttling that happens when a cgroup is above its
     // memory.high for too long. See more here:
     // https://github.com/neondatabase/autoscaling/issues/44#issuecomment-1522487217
     memory_high_increase_by_bytes: u64,
 
-    // memory_high_increase_every gives the period, in milliseconds, at which we should
-    // repeatedly increase the value of the cgroup's memory.high while we're waiting on upscaling
-    // and memory.high is still being hit.
+    // The period, in milliseconds, at which we should repeatedly increase the value
+    // of the cgroup's memory.high while we're waiting on upscaling and memory.high
+    // is still being hit.
     //
     // Technically speaking, this actually serves as a rate limit to moderate responding to
     // memory.high events, but these are roughly equivalent if the process is still allocating
@@ -228,7 +228,7 @@ impl Default for Config {
     }
 }
 
-/// Use to represent data that is associated with a certain point in time, such
+/// Used to represent data that is associated with a certain point in time, such
 /// as an upscale request or memory.high event.
 ///
 /// Internally, creating a `Sequenced` uses a static atomic counter to obtain
@@ -263,7 +263,7 @@ pub struct CgroupWatcher {
     /// The event stream that informs cgroup actions. Consists of upscale requests
     /// and `memory.high` events.
     ///
-    /// This is wrapped in a mutex because otherwise we need `&mut self` to
+    /// Note: this is wrapped in a mutex because otherwise we need `&mut self` to
     /// do anything useful. This is a problem because `main_signals_loop` never
     /// (or should never) return, so it effectively takes ownership of the
     /// `Cgroup`. However, the monitor still needs to be able to do things like
@@ -277,7 +277,7 @@ pub struct CgroupWatcher {
     /// `last_upscale_seqnum`, then we know it occured before the upscale, and we
     /// can safely ignore it.
     ///
-    /// Like the `events` field, this doesn't _need_ interior mutability but we
+    /// Note: Like the `events` field, this doesn't _need_ interior mutability but we
     /// use it anyways so that methods take `&self`, not `&mut self`.
     last_upscale_seqnum: AtomicU64,
 
@@ -316,11 +316,6 @@ fn get_event_count(path: &str, event: MemoryEvent) -> anyhow::Result<u64> {
     // ...
     contents
         .lines()
-        // REVIEW: `filter_map` means we're ignoring lines that don't have a
-        // space in them? Is that what we want? every line should have a space!
-        // (maybe to handle the trailing newline? but that's still ok!)
-        // felix=> not sure what you mean by this. All lines we care about
-        // have space and will get split correctly.
         .filter_map(|s| s.split_once(' '))
         .find(|(e, _)| *e == event.as_str())
         .ok_or_else(|| anyhow!("failed to find entry for memory.{event} events in {path}"))
@@ -351,6 +346,7 @@ fn create_file_watcher(path: &str) -> anyhow::Result<EventStream<[u8; 1024]>> {
 
 impl CgroupWatcher {
     #[tracing::instrument]
+    /// Create a new `CgroupWatcher`.
     pub fn new(
         name: String,
         // A channel on which upscale notifications will be sent
@@ -525,7 +521,8 @@ impl CgroupWatcher {
                             continue;
                         }
 
-                        // Request upscale anyways
+                        // Request upscale anyways (the agent will handle deduplicating
+                        // requests)
                         self.upscale_requester
                             .send(())
                             .await
