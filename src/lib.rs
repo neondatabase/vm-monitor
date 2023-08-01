dbfdbe67ea49dde1d3eb933b52416de37690bfa2
Felix Prasanna <felix@neon.tech>
2023-08-01 14:11:23 -0400

    move functionality into lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,28 @@
+use axum::{
+    extract::{ws::WebSocket, State, WebSocketUpgrade},
+    response::Response,
+};
 use clap::Parser;
-use std::fmt::Debug;
+use std::{fmt::Debug, sync::OnceLock, time::Duration};
 use sysinfo::{RefreshKind, System, SystemExt};
+use tokio::sync::broadcast;
+use tracing::{error, info};
+
+use crate::monitor::Monitor;
+
+// Code that interfaces with agent
+pub mod dispatcher;
+pub mod protocol;
+
+pub mod cgroup;
+pub mod filecache;
+pub mod monitor;
+
+/// The CLI arguments
+///
+/// This is in a static so we only have to parse it once and then the entire
+/// binary can see it.
+pub static ARGS: OnceLock<Args> = OnceLock::new();
 
 #[derive(Parser, Debug, Clone)]
 pub struct Args {
@@ -35,10 +57,53 @@ pub fn get_total_system_memory() -> u64 {
     System::new_with_specifics(RefreshKind::new().with_memory()).total_memory()
 }
 
-// Code that interfaces with agent
-pub mod dispatcher;
-pub mod protocol;
+/// Handles incoming websocket connections.
+///
+/// If we are already to connected to an informant, we kill that old connection
+/// and accept the new one.
+#[tracing::instrument(name = "/monitor", skip(ws))]
+pub async fn ws_handler(ws: WebSocketUpgrade, State(sender): State<broadcast::Sender<()>>) -> Response {
+    // Kill the old monitor
+    // TODO: this log is a little jank, as on the first connection to the server,
+    // we're not actually connected to anyone. We can probaly drop the first receiver
+    // and then look at the receiver count to determine if this if the first connection.
+    info!("already connected to an informant over websocket -> closing old connection");
+    let _ = sender.send(());
 
-pub mod cgroup;
-pub mod filecache;
-pub mod monitor;
+    // Start the new one. Wow, the cycle of death and rebirth
+    let closer = sender.subscribe();
+    ws.on_upgrade(|ws| start_monitor(ws, closer))
+}
+
+/// Starts the monitor. If startup fails or the monitor exits, an error will
+/// be logged and our internal state will be reset to allow for new connections.
+#[tracing::instrument(skip_all)]
+async fn start_monitor(ws: WebSocket, kill: broadcast::Receiver<()>) {
+    info!("accepted new websocket connection -> starting monitor");
+    let monitor = tokio::time::timeout(
+        Duration::from_secs(2),
+        // Unwrap is safe because we initialize at the beginning of main
+        Monitor::new(Default::default(), ARGS.get().unwrap(), ws, kill),
+    )
+    .await;
+    let mut monitor = match monitor {
+        Ok(Ok(monitor)) => monitor,
+        Ok(Err(error)) => {
+            error!(?error, "failed to create monitor");
+            return;
+        }
+        Err(elapsed) => {
+            error!(
+                ?elapsed,
+                "creating monitor timed out (probably waiting to receive protocol range)"
+            );
+            return;
+        }
+    };
+    info!("connected to informant");
+
+    match monitor.run().await {
+        Ok(()) => info!("monitor was killed due to new connection"),
+        Err(e) => error!(error = ?e, "monitor terminated by itself"),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,10 @@
-// REVIEW: is there a reason why src/main.rs is separate from src/lib.rs?
-// IMO it seems like unnecessary complication.
-use std::{sync::OnceLock, time::Duration};
-
 use anyhow::Context;
-use axum::{
-    extract::{ws::WebSocket, State, WebSocketUpgrade},
-    response::Response,
-    routing::get,
-    Router, Server,
-};
+use axum::{routing::get, Router, Server};
 use clap::Parser;
-use tokio::sync::broadcast;
-use tracing::{error, info, warn};
+use tracing::info;
 use tracing_subscriber::EnvFilter;
-use vm_monitor::monitor::Monitor;
 use vm_monitor::Args;
-
-static ARGS: OnceLock<Args> = OnceLock::new();
+use vm_monitor::{ws_handler, ARGS};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -43,8 +31,7 @@ async fn main() -> anyhow::Result<()> {
         .route("/monitor", get(ws_handler))
         .with_state(sender);
 
-    let args = Args::parse();
-    let addr = args.addr();
+    let addr = ARGS.get().unwrap().addr();
     let bound = Server::try_bind(&addr.parse().expect("parsing address should not fail"))
         .with_context(|| format!("failed to bind to {addr}"))?;
 
@@ -56,57 +43,4 @@ async fn main() -> anyhow::Result<()> {
         .context("server exited")?;
 
     Ok(())
-}
-
-/// Handles incoming websocket connections.
-///
-/// If we are already to connected to an informant, we kill that old connection
-/// and accept the new one.
-#[tracing::instrument(name = "/monitor", skip(ws))]
-async fn ws_handler(ws: WebSocketUpgrade, State(sender): State<broadcast::Sender<()>>) -> Response {
-    // Kill the old monitor
-    // TODO: this comment is a little jank, as on the first connection to the server,
-    // we're not actually connected to anyone. We cn probaly drop the first receiver
-    // and then look at the receiver count to determine if this if the first connection.
-    warn!("already connected to an informant over websocket -> closing old connection");
-    let _ = sender.send(());
-
-    // Start the new one. Wow, the cycle of death and rebirth
-    let closer = sender.subscribe();
-    ws.on_upgrade(|ws| start_monitor(ws, closer))
-}
-
-// TODO: should these warns be hard errors? In theory they can happen in normal
-// operation if we're switching agents
-/// Starts the monitor. If startup fails or the monitor exits, and error will
-/// be logged and our internal state will be reset to allow for new connections.
-#[tracing::instrument(skip_all)]
-async fn start_monitor(ws: WebSocket, kill: broadcast::Receiver<()>) {
-    info!("accepted new websocket connection -> starting monitor");
-    let monitor = tokio::time::timeout(
-        Duration::from_secs(2),
-        // Unwrap is safe because we initialize at the beginning of main
-        Monitor::new(Default::default(), ARGS.get().unwrap(), ws, kill),
-    )
-    .await;
-    let mut monitor = match monitor {
-        Ok(Ok(monitor)) => monitor,
-        Ok(Err(error)) => {
-            error!(?error, "failed to create monitor");
-            return;
-        }
-        Err(elapsed) => {
-            error!(
-                ?elapsed,
-                "creating monitor timed out (probably waiting to receive protocol range)"
-            );
-            return;
-        }
-    };
-    info!("connected to informant");
-
-    match monitor.run().await {
-        Ok(()) => info!("monitor was killed due to new connection"),
-        Err(e) => error!(error = ?e, "monitor terminated by itself"),
-    }
 }
